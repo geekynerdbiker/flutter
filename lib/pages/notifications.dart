e9adc4a32494103c1c5142302451c02394b2d620
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-04-23 08:09:04 +0900

    update

---
+++ lib/pages/notifications.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:newnew/models/post.dart';
+import 'package:newnew/models/user.dart';
+import 'package:newnew/models/comment.dart';
+import 'package:newnew/models/global.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
