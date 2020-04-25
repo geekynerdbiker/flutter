6250594ef54bacf6d3899f65bf1da5c3f5753c2f
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-04-25 12:26:57 +0900

    Create testInput.dart

---
+++ lib/testInput.dart
@@ -0,0 +1,73 @@
+import 'package:newnewmarket/components/collections/exclusiveCollections.dart';
+import 'package:newnewmarket/components/collections/featuredCollections.dart';
+
+import 'components/home/popularDesigners.dart';
+import 'components/home/shopLatestTrends.dart';
+import 'components/home/shopByCategory.dart';
+import 'components/home/shopByStyle.dart';
+import 'components/home/trendingSearch.dart';
+import 'components/collections/communityCollections.dart';
+
+List<Trend> trendItems = [
+  new Trend('Nike'),
+  new Trend('Adidas'),
+  new Trend('Vans'),
+  new Trend('Converse'),
+  new Trend('Golden Goose'),
+  new Trend('Test1'),
+  new Trend('Test2'),
+];
+
+List<Category> categoryItems = [
+  new Category('Outerwear'),
+  new Category('Top'),
+  new Category('Bottom'),
+  new Category('Footwear'),
+  new Category('Tailoring'),
+  new Category('Accesories'),
+];
+
+List<Style> styleItems = [
+  new Style('Street'),
+  new Style('Vintage'),
+  new Style('Luxury'),
+  new Style('Casual'),
+  new Style('Avant Garde'),
+  new Style('Minimalist'),
+];
+
+List<LatestTrend> latestTrendItems = [
+  new LatestTrend('Cardigans'),
+  new LatestTrend('Cargo Pants'),
+  new LatestTrend('Chore Jackets'),
+];
+
+List<Designer> designerItems = [
+  new Designer('Supreme'),
+  new Designer('Palace'),
+  new Designer('Raf Simons'),
+  new Designer('GUCCI'),
+  new Designer('Balenciaga'),
+  new Designer('Off-White'),
+];
+
+List<CommunityCollection> cCollectionItems = [
+  new CommunityCollection('C Collection1'),
+  new CommunityCollection('C Collection2'),
+  new CommunityCollection('C Collection3'),
+  new CommunityCollection('C Collection4'),
+];
+
+List<ExclusiveCollection> eCollectionItems = [
+  new ExclusiveCollection('C Collection1'),
+  new ExclusiveCollection('C Collection2'),
+  new ExclusiveCollection('C Collection3'),
+  new ExclusiveCollection('C Collection4'),
+];
+
+List<FeaturedCollection> fCollectionItems = [
+  new FeaturedCollection('C Collection1'),
+  new FeaturedCollection('C Collection2'),
+  new FeaturedCollection('C Collection3'),
+  new FeaturedCollection('C Collection4'),
+];
