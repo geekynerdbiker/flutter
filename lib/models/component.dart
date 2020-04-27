8b46e4a76fa99fa0b7f066f18de5e65b836c2383
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-04-27 23:54:21 +0900

    Update build

---
+++ lib/models/component.dart
@@ -0,0 +1,34 @@
+class Category {
+  String title;
+  String imageURI;
+
+  Category(this.title);
+}
+
+class Collection {
+  String title;
+  String tagURI;
+
+  Collection(this.title);
+}
+
+class Designer {
+  String title;
+  String imageURI;
+
+  Designer(this.title);
+}
+
+class Style {
+  String title;
+  String imageURI;
+
+  Style(this.title);
+}
+
+class Trend {
+  String title;
+  String tagURI;
+
+  Trend(this.title);
+}
