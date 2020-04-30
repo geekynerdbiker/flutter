a0fbf6ceecf8ace770497675a1509bab927451a1
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-04-30 12:48:03 +0900

    Update widgets

--- lib/widgets/collection/mainPage/collections.dart
+++ lib/widgets/collection/mainPage/collections.dart
@@ -9,19 +9,18 @@ class Collections extends StatelessWidget {
     List<Widget> collections = [];
     int index = 0;
     for (Collection collection in collectionItems) {
-      collections.add(getCommunityCollection(context, collection, index));
+      collections.add(getCollection(context, collection, index));
       index ++;
     }
     return collections;
   }
 
-  Widget getCommunityCollection(BuildContext context, Collection collections, int index) {
+  Widget getCollection(BuildContext context, Collection collections, int index) {
     return Padding(
-      padding: EdgeInsets.only(left: 10),
+      padding: EdgeInsets.all(10),
       child: Container(
         width: 250,
-        height: 100,
-        padding: EdgeInsets.only(top: 5, left: 8, right: 8),
+        height: 200,
         decoration: BoxDecoration(
             border: Border.all(color: Colors.black)
         ),
@@ -42,10 +41,9 @@ class Collections extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 180,
+      height: 200,
       child: ListView(
-        padding: EdgeInsets.only(left: 5, right: 15),
-        scrollDirection: Axis.horizontal,
+        scrollDirection: Axis.vertical,
         children:
         getCollections(context),
       ),
