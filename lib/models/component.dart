670bccff0fdb091a87354b3e060aba20683165df
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-05-18 21:10:28 +0900

    Update models

--- lib/models/component.dart
+++ lib/models/component.dart
@@ -29,16 +29,9 @@ class Style {
   Style(this.title);
 }
 
-class Trend {
-  String title;
-  String tagURI;
-
-  Trend(this.title);
-}
-
 class Product {
   String title;
-  AssetImage image;
+  String imageURI;
   Category category1;
   Category category2;
   Category category3;
@@ -51,6 +44,14 @@ class Product {
   bool isLiked;
   bool isSaved;
 
-  Product(this.title, this.category1, this.category2, this.category3, this.price);
+  Product(this.title, this.imageURI, this.category1, this.category2, this.category3, this.price);
   //Product(this.image, this.user, this.description, this.date, this.likes, this.comments, this.isLiked, this.isSaved);
+}
+
+class ProductList {
+  List<Product> myItems;
+  List<Product> recentView;
+  List<Product> wishList;
+
+  ProductList(this.myItems, this.recentView, this.wishList);
 }
