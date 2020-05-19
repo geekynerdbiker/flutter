d69b1992311e007da31330391d8908ed365736b0
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-05-19 19:30:48 +0900

    Update component.dart

--- lib/models/component.dart
+++ lib/models/component.dart
@@ -1,5 +1,36 @@
 import 'package:flutter/material.dart';
-import 'user.dart';
+
+class User {
+  String username;
+  List<Post> posts;
+  AssetImage profilePicture;
+  List<User> followers;
+  List<User> following;
+  List<Post> savedPosts;
+
+  User(this.username, this.profilePicture, this.followers, this.following, this.posts, this.savedPosts);
+}
+
+class Post {
+  AssetImage image;
+  String description;
+  User user;
+  List<User> likes;
+  List<Comment> comments;
+  DateTime date;
+  bool isLiked;
+  bool isSaved;
+
+  Post(this.image, this.user, this.description, this.date, this.likes, this.comments, this.isLiked, this.isSaved);
+}
+
+class Comment {
+  String comment;
+  User user;
+  DateTime dateOfComment;
+  bool isLiked;
+  Comment(this.user, this.comment, this.dateOfComment, this.isLiked);
+}
 
 class Category {
   String title;
@@ -48,10 +79,10 @@ class Product {
   //Product(this.image, this.user, this.description, this.date, this.likes, this.comments, this.isLiked, this.isSaved);
 }
 
-class ProductList {
+class ProductLists {
   List<Product> myItems;
   List<Product> recentView;
   List<Product> wishList;
 
-  ProductList(this.myItems, this.recentView, this.wishList);
+  ProductLists(this.myItems, this.recentView, this.wishList);
 }
