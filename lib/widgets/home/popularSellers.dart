fe137c08cec0b0aab0561290aabd6d32b7d50e83
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-05-19 19:33:45 +0900

    Update widgets

--- lib/widgets/home/popularSellers.dart
+++ lib/widgets/home/popularSellers.dart
@@ -19,16 +19,25 @@ class PopularSellers extends StatelessWidget {
   Widget getUser(BuildContext context, User user, int index) {
     return Padding(
       padding: EdgeInsets.only(left: 10),
-      child: Container(
-        width: 100,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(100),
-          color: Colors.black,
-        ),
-        child: Center(
-          child: Text(user.username, style: TextStyle(color: Colors.yellow)),
-        ),
-        ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Colors.black,
+            ),
+            child: Center(
+              child: Text(user.username, style: TextStyle(color: Colors.yellow)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10),
+            child: Text(user.username, style: TextStyle(fontWeight: FontWeight.bold),),
+          )
+        ],
+      )
       );
   }
 
