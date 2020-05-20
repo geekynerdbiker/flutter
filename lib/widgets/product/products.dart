967cb487b62e0adeb18a169b8aff87c9116c1718
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-05-21 08:46:51 +0900

    Update widgets

--- lib/widgets/product/products.dart
+++ lib/widgets/product/products.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newnew/pages/product/productDetail.dart';
 
 import 'package:newnew/testInput.dart';
 import 'package:newnew/models/component.dart';
@@ -17,61 +18,70 @@ class Products extends StatelessWidget {
   Widget getProduct(BuildContext context, Product product, int index) {
     return Padding(
       padding: EdgeInsets.all(5),
-      child: Container(
-        decoration: BoxDecoration(border: Border.all(color: Colors.black)),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 235,
-              decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Colors.black))),
+      child: Material(
+        child: InkWell(
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              '/b');
+          },
+          child: Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 235,
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: Colors.black))),
+                ),
+                Container(
+                  height: 80,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: 30,
+                        width: 170,
+                        child: Text(
+                          product.category1.title,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 25,
+                        width: 170,
+                        child: Text(
+                          product.title,
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 25,
+                        width: 170,
+                        child: Text(
+                          product.price.toString() + '원',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              height: 80,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: 30,
-                    width: 170,
-                    child: Text(
-                      product.category1.title,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 25,
-                    width: 170,
-                    child: Text(
-                      product.title,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 25,
-                    width: 170,
-                    child: Text(
-                      product.price.toString() + '원',
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
+      )
     );
   }
 
