301297ff0e99ac1233ad5581a65cc31a5c26ebf1
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-01 21:24:37 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:newnew/pages/product/addProductPage.dart';
 
 import 'package:newnew/pages/product/productListPage.dart';
 import 'package:newnew/pages/category/mainPage.dart';
+import 'package:newnew/pages/start/splashScreen.dart';
 
 
 import 'pages/home/mainPage.dart';
@@ -21,59 +22,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: SplashScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 5,
-      initialIndex: 0,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: TabBarView(
-          children: <Widget> [
-            HomePage(),
-            CollectionPage(),
-            AddProductPage(),
-            CategoryPage(),
-            ProfilePage(),
-          ],
-        ),
-        bottomNavigationBar: Container(
-          child: new TabBar(
-            labelColor: Colors.black,
-            indicatorColor: Colors.black,
-            unselectedLabelColor: Colors.grey,
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home),
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-              ),
-              Tab(
-                icon: Icon(Icons.add),
-              ),
-              Tab(
-                icon: Icon(Icons.view_headline),
-              ),
-              Tab(
-                icon: Icon(Icons.perm_identity),
-              ),
-            ],
-
-          ),
-        ),
-      ),
-    );
-  }
-}
