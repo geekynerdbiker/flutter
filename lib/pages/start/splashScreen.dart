6f23d05645a7f01ebe7e1d797b89fec2ace2de3b
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-01 21:37:45 +0900

    Update pages

--- lib/pages/start/splashScreen.dart
+++ lib/pages/start/splashScreen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:newnew/pages/home/mainPage.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -6,6 +8,24 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+  startTime() async {
+    var duration = new Duration(seconds: 3);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => HomePage()
+    )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
