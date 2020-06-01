c1809442c47fba3f869c6edc8be24148756371a2
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-01 21:24:31 +0900

    Update build info

---
+++ lib/pages/start/splashScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width,
+          child: Image.asset('lib/assets/newnew_logo.png'),
+        ),
+      )x2wys78\
+  =]-['0p;;;;QP-[-='0P;IQ]345p0-
+    =['-p0o9i8u7=-0]9[p876543Z85']);
+  }
+}
