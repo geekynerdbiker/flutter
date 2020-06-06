7dca2d5c8b182a440c044e9b3e9f6f0f545eb6b5
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-06 23:09:15 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:newnew/pages/forYou/userCollectionPage.dart';
-import 'package:newnew/pages/etc/popularSeller.dart';
-import 'package:newnew/pages/product/addProductPage.dart';
-
-import 'package:newnew/pages/product/productListPage.dart';
-import 'package:newnew/pages/category/mainPage.dart';
 import 'package:newnew/pages/start/splashScreen.dart';
-
-
-import 'pages/home/mainPage.dart';
-import 'pages/forYou/mainPage.dart';
-import 'pages/profile/mainPage.dart';
-import 'pages/product/productListPage.dart';
-
 
 void main() => runApp(MyApp());
 
