89d69b6542dfa0fede5d4a2cc7b9349c4b093a5b
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-17 17:54:40 +0900

    Update design system

--- lib/models/const/sizeSystem.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-// const fullWidth = const MediaQuery.of(context).size.width;
-// const fullHeight = const MediaQuery.of(context).size.height;
---
+++ lib/models/designSystem/sizeSystem.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+// designSystem fullWidth = designSystem MediaQuery.of(context).size.width;
+// designSystem fullHeight = designSystem MediaQuery.of(context).size.height;
