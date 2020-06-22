6b84bbc00bd98219ab429045c3e9c5d3f456272c
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-22 18:53:15 +0900

    Update widgets

--- lib/widgets/profile/myCollections.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:newnew/pages/forYou/collectionDetailPage.dart';
-
-import 'package:newnew/testInput.dart';
-import 'package:newnew/models/classes/collection.dart';
-import 'package:newnew/widgets/forYou/sideScrollViewerVertical.dart';
-
-
