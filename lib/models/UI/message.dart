652e13abb88d1abd8f1855e32316ae757158a70d
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-25 21:25:39 +0900

    Update models

--- lib/models/UI/message.dart
+++ lib/models/UI/message.dart
@@ -12,60 +12,59 @@ Widget messageListItem(BuildContext context, User user) {
   const double _iSpace = 20;
 
   return Container(
-      height: MediaQuery.of(context).size.width * (84 / 375),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 20,
-              ),
-              Container(
-                width: _r,
-                height: _r,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.grey),
-                child: Center(
-                    child: Image.asset(
-                  user.imageURI,
-                  fit: BoxFit.cover,
-                )),
-              ),
-              Container(
-                width: _space,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(user.username),
-
-                      Container(width: _tSpace1),
-                     Text(user.lastActivity.toString() + '전'),
-                    ],
-                  ),
-                  Container(
-                    height: _tSpace2,
-                  ),
-                  Text('Message Text'),
-                ],
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: _iSpace),
-            child: Container(
-              width: _iSize,
-              height: _iSize,
-              color: Colors.grey,
+    height: MediaQuery.of(context).size.width * (84 / 375),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 20,
             ),
-          )
-        ],
-      ));
+            Container(
+              width: _r,
+              height: _r,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50), color: Colors.grey),
+              child: Center(
+                  child: Image.asset(
+                user.imageURI,
+                fit: BoxFit.cover,
+              )),
+            ),
+            Container(
+              width: _space,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(user.username),
+                    Container(width: _tSpace1),
+                    Text(user.lastActivity.toString() + '전'),
+                  ],
+                ),
+                Container(
+                  height: _tSpace2,
+                ),
+                Text('Message Text'),
+              ],
+            ),
+          ],
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: _iSpace),
+          child: Container(
+            width: _iSize,
+            height: _iSize,
+            color: Colors.grey,
+          ),
+        )
+      ],
+    ),
+  );
 }
