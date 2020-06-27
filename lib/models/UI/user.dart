79a917d9b44294813d5823d45b8f85da3a4a5488
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-06-27 19:01:42 +0900

    Update models

--- lib/models/UI/user.dart
+++ lib/models/UI/user.dart
@@ -1,34 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:editsource/models/classes/user.dart';
 
-Widget popularSeller(BuildContext context, User user) {
+Widget userMarqueePopularSeller(BuildContext context, User user) {
   const double _r = 64;
   const double _space = 8;
-
-  const double _tHeight = 15;
 
   return Container(
     child: Column(
       children: <Widget>[
-        Container(
-          width: _r,
-          height: _r,
-          color: Colors.grey,
-          child: Center(
-              child: Image.asset(
-                user.imageURI,
-                fit: BoxFit.cover,
-              )),
-        ),
-        Container(
-          height: _space,
-        ),
-        Container(
-            width: _r,
-            height: _tHeight,
-            child: Center(
-              child: Text(user.username),
-            ))
+        profileImage(user, _r),
+        hSpacer(_space),
+        Text('@' + user.username),
       ],
     ),
   );
@@ -36,187 +18,121 @@ Widget popularSeller(BuildContext context, User user) {
 
 Widget userMarqueeMyPage(BuildContext context, User user) {
   const double _r = 48;
-  const double _space = 12;
-
-  const double _tWidth = 100;
-  const double _tHeight = 15;
-  const double _tSpace = 12;
-
-  const double _bWidth = 58;
-  const double _bHeight = 15;
-
-  const double _iSize1 = 11;
-  const double _iSize2 = 24;
-  const double _iSpace1 = 4;
-  const double _iSpace2 = 12;
+  const double _space1 = 12;
+  const double _space2 = 12;
+  const double _space3 = 4;
+  const double _space4 = 12;
 
   return Container(
     child: Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Container(
-          width: _r,
-          height: _r,
-          color: Colors.grey,
-          child: Center(
-              child: Image.asset(
-                user.imageURI,
-                fit: BoxFit.cover,
-              )),
-        ),
-        Container(
-          width: _space,
-        ),
-        Column(
-          children: [
-            Container(
-                width: _tWidth,
-                height: _tHeight,
-                child: Center(
-                  child: Text(user.bio),
-                )),
-            Container(height: _tSpace),
-            Row(
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            profileImage(user, _r),
+            wSpacer(_space1),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                    width: _iSize1,
-                    height: _iSize1,
-                    child: Center(
-                        child: Icon(Icons.watch_later)
-                    )),
-                Container(
-                  width: _iSpace1,
-                ),
-                Container(
-                    width: _bWidth,
-                    height: _bHeight,
-                    child: Center(
-                      child: Text(user.lastActivity.toString()),
-                    )),
-                Container(
-                  width: _iSpace2,
-                ),
-                Container(
-                    width: _iSize1,
-                    height: _iSize1,
-                    child: Center(
-                        child: Icon(Icons.label)
-                    )),
-                Container(
-                  width: _iSpace1,
-                ),
-                Container(
-                    width: _bWidth,
-                    height: _bHeight,
-                    child: Center(
-                      child: Text(user.myProducts.toString()),
-                    )),
-                Container(
-                  width: _iSpace2,
-                ),
-                Container(
-                    width: _iSize1,
-                    height: _iSize1,
-                    child: Center(
-                        child: Icon(Icons.directions_car)
-                    )),
-                Container(
-                  width: _iSpace1,
-                ),
-                Container(
-                    width: _bWidth,
-                    height: _bHeight,
-                    child: Center(
-                      child: Text('Delivery'),
-                    )),
+                Text('user.bio'),
+                hSpacer(_space2),
+                Row(
+                  children: [
+                    Icon(Icons.watch_later),
+                    wSpacer(_space3),
+                    Text(user.lastActivity.toString()),
+                    wSpacer(_space4),
+                    Icon(Icons.label),
+                    wSpacer(_space3),
+                    Text(user.myProducts.toString()),
+                    wSpacer(_space4),
+                    Icon(Icons.directions_car),
+                    wSpacer(_space3),
+                    Text('Delivery'),
+                  ],
+                )
               ],
-            )
+            ),
           ],
         ),
-        Container(
-          width: _iSize2,
-          height: _iSize2,
-          child: Icon(Icons.edit),
-        )
+        Icon(Icons.edit),
       ],
     ),
   );
 }
 
+Widget userMarqueeMessageListItem(BuildContext context, User user) {
+  const double _r = 48;
+  const double _space1 = 12;
+  const double _space2 = 8;
+  const double _space3 = 13;
+
+  const double _iSize = 52;
+
+  return Container(
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            profileImage(user, _r),
+            wSpacer(_space1),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(user.username),
+                    wSpacer(_space2),
+                    Text(user.lastActivity.toString() + '전'),
+                  ],
+                ),
+                hSpacer(_space3),
+                Text('Message Text'),
+              ],
+            ),
+          ],
+        ),
+        Container(
+          width: _iSize,
+          height: _iSize,
+          color: Colors.grey,
+        ),
+      ],
+    ),
+  );
+}
 
 Widget userMarquee1(BuildContext context, User user) {
   const double _r = 48;
-  const double _space = 12;
-
-  const double _tWidth = 100;
-  const double _tHeight = 15;
-  const double _tSpace = 8;
-
-  const double _bWidth = 32;
-  const double _bHeight = 15;
-  const double _bSpace1 = 4;
-  const double _bSpace2 = 16;
+  const double _space1 = 12;
+  const double _space2 = 8;
+  const double _space3 = 4;
+  const double _space4 = 16;
 
   return Container(
     child: Row(
       children: <Widget>[
-        Container(
-          width: _r,
-          height: _r,
-          color: Colors.grey,
-          child: Center(
-              child: Image.asset(
-                user.imageURI,
-                fit: BoxFit.cover,
-              )),
-        ),
-        Container(
-          width: _space,
-        ),
+        profileImage(user, _r),
+        wSpacer(_space1),
         Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-                width: _tWidth,
-                height: _tHeight,
-                child: Center(
-                  child: Text(user.username),
-                )),
-            Container(height: _tSpace),
+            Text('@' + user.username),
+            hSpacer(_space2),
             Row(
               children: [
-                Container(
-                    width: _bWidth,
-                    height: _bHeight,
-                    child: Center(
-                      child: Text('아이템'),
-                    )),
-                Container(
-                  width: _bSpace1,
-                ),
-                Container(
-                    width: _bWidth,
-                    height: _bHeight,
-                    child: Center(
-                      child: Text(user.myProducts.length.toString()),
-                    )),
-                Container(
-                  width: _bSpace2,
-                ),
-                Container(
-                    width: _bWidth,
-                    height: _bHeight,
-                    child: Center(
-                      child: Text('팔로워'),
-                    )),
-                Container(
-                  width: _bSpace1,
-                ),
-                Container(
-                    width: _bWidth,
-                    height: _bHeight,
-                    child: Center(
-                      child: Text(user.followers.length.toString()),
-                    )),
+                Text('아이템'),
+                wSpacer(_space3),
+                Text(user.myProducts.length.toString()),
+                wSpacer(_space4),
+                Text('팔로워'),
+                wSpacer(_space3),
+                Text(user.followers.length.toString()),
               ],
             )
           ],
@@ -227,75 +143,57 @@ Widget userMarquee1(BuildContext context, User user) {
 }
 
 Widget userMarquee2(BuildContext context, User user) {
-  const double _width = 375;
-  const double _height = 40;
-
   const double _r = 40;
-  const double _space = 20;
-
-  const double _tWidth = 140;
-  const double _tHeight = 21;
-  const double _tSpace = 12;
-
-  const double _bWidth1 = 20;
-  const double _bWidth2 = 90;
-  const double _bHeight = 17;
-  const double _bSpace1 = 4;
-  const double _bSpace2 = 8;
-
-  const double _iSize = 4;
+  const double _space1 = 12;
+  const double _space2 = 4;
+  const double _space3 = 8;
 
   return Container(
-    width: _width,
-    height: _height,
-    child: Row(
-      children: <Widget>[
-        Container(width: _space,),
-        Container(
-          width: _r,
-          height: _r,
-          color: Colors.grey,
-          child: Center(
-              child: Image.asset(
-                user.imageURI,
-                fit: BoxFit.cover,
-              )),
-        ),
-        Container(
-          width: _tSpace,
-        ),
-        Container(
-            width: _tWidth,
-            height: _tHeight,
-            child: Center(
-              child: Text(user.username),
-            )),
-        Container(height: _tSpace),
-        Container(
-            width: _iSize,
-            height: _iSize,
-            child: Center(
-              child: Icon(Icons.star_border)
-            )),
-        Container(
-          width: _bSpace1,
-        ),
-        Container(
-            width: _bWidth1,
-            height: _bHeight,
-            child: Center(
-              child: Text(user.getRate().toString()),
-            )),
-        Container(
-          width: _bSpace2,
-        ),
-        Container(
-            width: _bWidth2,
-            height: _bHeight,
-            child: Center(
-              child: Text('(' + user.getReviews().toString() + ')' + ' Reviews'),
-            )),
-      ],
-    )
+      child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: <Widget>[
+      Row(
+        children: [
+          profileImage(user, _r),
+          wSpacer(_space1),
+          Text(user.username),
+        ],
+      ),
+      Row(
+        children: [
+          Icon(Icons.star_border),
+          wSpacer(_space2),
+          Text(user.getRate().toString()),
+          wSpacer(_space3),
+          Text('(' + user.getReviews().toString() + ' Reviews' + ')'),
+        ],
+      )
+    ],
+  ));
+}
+
+Widget profileImage(User user, double _r) {
+  return Container(
+    width: _r,
+    height: _r,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50), color: Colors.grey),
+    child: Center(
+        child: Image.asset(
+      user.imageURI,
+      fit: BoxFit.cover,
+    )),
+  );
+}
+
+Widget wSpacer(double _space) {
+  return Container(
+    width: _space,
+  );
+}
+
+Widget hSpacer(double _space) {
+  return Container(
+    height: _space,
   );
 }
