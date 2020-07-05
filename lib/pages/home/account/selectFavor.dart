c5b4716afc803d07eceabe37c3708f31f90d7410
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-07-05 20:28:10 +0900

    Update pages

--- lib/pages/home/account/selectFavor.dart
+++ lib/pages/home/account/selectFavor.dart
@@ -1,43 +1,43 @@
 import 'package:editsource/models/designs/colors.dart';
 import 'package:editsource/models/components/navigation.dart';
 import 'package:editsource/pages/home/account/selectFavor2.dart';
+import 'package:editsource/pages/home/bootPage.dart';
 import 'package:flutter/material.dart';
 
 class SelectFavorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBarWithSkip(context, '취향 선택', SelectFavorPage2()),
+      appBar: appBarWithSkip(context, '취향 선택', BootPage()
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        width: MediaQuery.of(context).size.width * (335 / 375),
+        height: 44,
+        child: FloatingActionButton.extended(
+            heroTag: "confirm1",
+            label: Text(
+              "다음 단계",
+              style: TextStyle(color: Colors.white),
+            ),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0))),
+            backgroundColor: Colors.black,
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SelectFavorPage2()));
+            }),
+      ),
       backgroundColor: offWhite,
       body: Container(
-        width: MediaQuery.of(context).size.width,
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
         child: Column(
           children: <Widget>[
-            basicNavigationButton(context, '다음 단계', SelectFavorPage2()),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget basicNavigationButton(
-      BuildContext context, String _textContext, Widget _route) {
-    return Material(
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => _route));
-        },
-        child: Container(
-          height: 44,
-          width: MediaQuery.of(context).size.width * (335 / 375),
-          color: Colors.black,
-          child: Center(
-            child: Text(
-              _textContext,
-              style: TextStyle(fontSize: 14, color: offWhite),
-            ),
-          ),
         ),
       ),
     );
