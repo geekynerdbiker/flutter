235359dcf79e39725fc3cf2335c39a3ca5d1d4da
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-07-07 23:45:48 +0900

    Updtae database

--- lib/database/initialize.dart
+++ lib/database/initialize.dart
@@ -1,106 +1,126 @@
 import 'package:bak/models/classes/collection.dart';
 import 'package:bak/models/classes/product.dart';
 import 'package:bak/models/classes/user.dart';
+import 'package:bak/models/components/cards.dart';
+import 'package:bak/models/designs/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class FirestoreCRUD extends StatefulWidget {
-  String collectionName;
-
-  FirestoreCRUD({Key key, this.collectionName}) : super(key: key);
-
-  @override
-  FirestoreCRUDState createState() => FirestoreCRUDState();
+Widget productItemList(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('products').snapshots(),
+    builder: (context, snapshot) {
+      if(!snapshot.hasData) return CircularProgressIndicator();
+      return buildBody(context, snapshot.data.documents);
+    },
+  );
 }
 
-class FirestoreCRUDState extends State<FirestoreCRUD> {
-  Firestore firestore = Firestore.instance;
-  Stream<QuerySnapshot> streamData;
+List<Product> getProductItemList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return snapshot.map((e) => Product.fromSnapshot(e)).toList();
+}
 
-  @override
-  void initState() {
-    super.initState();
-    streamData = firestore.collection(widget.collectionName).snapshots();
-  }
+Widget collectionItemList(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('collections').snapshots(),
+    builder: (context, snapshot) {
+      if(!snapshot.hasData) return CircularProgressIndicator();
+      return buildBody(context, snapshot.data.documents);
+    },
+  );
+}
 
-//  Widget fetchData(BuildContext context, String collectionName) {
-//    return StreamBuilder<QuerySnapshot>(
-//      stream: Firestore.instance.collection(collectionName).snapshots(),
-//      builder: (context, snapshot) {
-//        if (!snapshot.hasData) return LinearProgressIndicator();
-//        return buildBody(context, snapshot.data.documents, collectionName);
-//      },
-//    );
-//  }
+List<Collection> getCollectionItemList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return snapshot.map((e) => Collection.fromSnapshot(e)).toList();
+}
 
-  List<User> getUserList(
-      BuildContext context, List<DocumentSnapshot> snapshot) {
-    List<User> users = snapshot.map((d) => User.fromSnapshot(d)).toList();
-    return users;
-  }
+Widget userItemList(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('users').snapshots(),
+    builder: (context, snapshot) {
+      if(!snapshot.hasData) return CircularProgressIndicator();
+      return buildBody(context, snapshot.data.documents);
+    },
+  );
+}
 
-  List<Collection> getCollectionList(
-      BuildContext context, List<DocumentSnapshot> snapshot) {
-    List<Collection> collections =
-        snapshot.map((d) => Collection.fromSnapshot(d)).toList();
-    return collections;
-  }
+List<User> getUserItemList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return snapshot.map((e) => User.fromSnapshot(e)).toList();
+}
 
-  List<Product> getProductList(
-      BuildContext context, List<DocumentSnapshot> snapshot) {
-    List<Product> products =
-        snapshot.map((d) => Product.fromSnapshot(d)).toList();
-    return products;
-  }
+Widget tagsItemList(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('tags').snapshots(),
+    builder: (context, snapshot) {
+      if(!snapshot.hasData) return CircularProgressIndicator();
+      return buildBody(context, snapshot.data.documents);
+    },
+  );
+}
 
-  List<Review> getReviewList(
-      BuildContext context, List<DocumentSnapshot> snapshot) {
-    List<Review> reviews = snapshot.map((d) => Review.fromSnapshot(d)).toList();
-    return reviews;
-  }
+List<Tag> getTagItemList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return snapshot.map((e) => Tag.fromSnapshot(e)).toList();
+}
 
-  List<Tag> getTagList(BuildContext context, List<DocumentSnapshot> snapshot) {
-    List<Tag> tags = snapshot.map((d) => Tag.fromSnapshot(d)).toList();
+Widget reviewsItemList(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('reviews').snapshots(),
+    builder: (context, snapshot) {
+      if(!snapshot.hasData) return CircularProgressIndicator();
+      return buildBody(context, snapshot.data.documents);
+    },
+  );
+}
 
-    return tags;
-  }
+List<Review> getReviewItemList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return snapshot.map((e) => Review.fromSnapshot(e)).toList();
+}
 
-  List<Category> getCategoryList(
-      BuildContext context, List<DocumentSnapshot> snapshot) {
-    List<Category> categories =
-        snapshot.map((d) => Category.fromSnapshot(d)).toList();
-    return categories;
-  }
+Widget buildBody(BuildContext context, List<DocumentSnapshot> snapshot) {
+  List<Product> productItems =
+      snapshot.map((e) => Product.fromSnapshot(e)).toList();
+  return Expanded(
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+      itemCount: productItems.length,
+      itemBuilder: (context, index) {
+        return productItemCardMedium(context, productItems[index], offWhite);
+      },
+    ),
+  );
+}
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        child: FutureBuilder(
-            future: getProducts(),
-            builder: (_, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: Text('Loading..'),
-                );
-              } else {
-                return ListView.builder(itemBuilder: (_, index) {
-                  return ListTile(
-                    title: Text(snapshot.data[index].data['title']),
-                  );
-                });
-              }
-            }));
-  }
+void addProduct() {
+  Firestore.instance.collection('products').add(
+      {
+        "userID" : "admin",
+        "title" : "마르지엘라 흰색 타비 부츠",
+        "description" : "송아지가죽 소재의 흰색 타비 부츠입니다. 굽8cm이지만 그렇게 높게 느껴지지않아요. 5번정도 신었음. 스크래치 거의 없고, 상태 양호.",
+        "updateDate" : "2020/07/07",
+        "soldDate" : "2020/07/07",
+        "status" : 0,
+        "price" : 480000,
+        "deliveryFee" : 4000,
+        "state" : 0,
+        "size" : "",
+        "material" : "",
+       // "color" = FieldValue.arrayUnion(String s);
+        "isLiked" : false,
+        "category" : "여성 잡화",
+        "rate" : 0.0
+      });
+}
 
-  Future getProducts() async {
-    var firestore = Firestore.instance;
-    QuerySnapshot qn = await firestore.collection('products').getDocuments();
-    return qn.documents;
-  }
-
-  Future getCollections() async {
-    var firestore = Firestore.instance;
-    QuerySnapshot qn = await firestore.collection('collections').getDocuments();
-    return qn.documents;
-  }
+void addCollection() {
+  Firestore.instance.collection('collections').add (
+    {
+      'userID' : '',
+      'title' : '내맘대로 할거임',
+      'imageURI' : '',
+      'description' : '',
+      'followers' : {},
+      'products' : {},
+      'tags' : {}
+    }
+  );
 }
