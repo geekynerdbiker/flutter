e00088822f46e34a1b8202048938938e50aafc47
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2020-07-09 17:52:04 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
-import 'package:bak/pages/home/bootPage.dart';
 import 'package:bak/pages/home/splashScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-
-const start = '/';
-const mainPage = '/mainPage';
-
-final routes = <String, WidgetBuilder> {
-  start: (BuildContext context) => SplashScreen(),
-  mainPage: (BuildContext context) => BootPage(),
-};
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: start,
-      routes: routes,
+      home: SplashScreen(),
     );
   }
 }
