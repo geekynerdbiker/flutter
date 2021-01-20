8716bd854ab68113657a80c1394042ef619beca5
Jacob An <lightyear.thespaceranger@gmail.com>
2021-01-20 14:01:21 +0900

    Create splash.dart

---
+++ artpia/lib/pages/start/splash.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+import 'package:artpia/pages/start/onboarding.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = new Duration(seconds: 2);
+    return new Timer(duration, route);
+  }
+
+  route() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => OnboardingScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.width,
+          child: Image.asset(
+            'lib/assets/logo_path',
+            scale: 0.5,
+          ),
+        ),
+      ),
+    );
+  }
+}
