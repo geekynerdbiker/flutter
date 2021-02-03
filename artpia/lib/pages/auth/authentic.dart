57705f559cf963606d09f04ba4ccd4bd362e4ef7
Jacob An <lightyear.thespaceranger@gmail.com>
2021-02-03 17:56:38 +0900

    Update authentic.dart

--- artpia/lib/pages/auth/authentic.dart
+++ artpia/lib/pages/auth/authentic.dart
@@ -7,7 +7,25 @@ class AuthenticPage extends StatefulWidget {
 class _AuthenticPageState extends State<AuthenticPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        children: [Image.asset('logo_main'), Button(context, 'Sign In')],
+      ),
+    );
+  }
+
+  Widget Button(BuildContext context, String title) {
+    return Container(
+      width: (MediaQuery.of(context).size.width - 20) / 2,
+      height: 40,
+      child: Center(
+        child: Text(
+          title,
+          style: TextStyle(),
+        ),
+      ),
+    );
   }
 }
