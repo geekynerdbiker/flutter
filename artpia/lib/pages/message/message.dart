060da41f2c0d265c5faa221ae87afedafa86964d
Jacob An <lightyear.thespaceranger@gmail.com>
2021-02-10 20:51:58 +0900

    Create pages

---
+++ artpia/lib/pages/message/message.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:artpia/pages/message/modules.dart';
+
+class MessagePage extends StatefulWidget {
+  @override
+  _MessagePageState createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: messageAppBar(context),
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(15),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              color: Colors.yellow,
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              color: Colors.green,
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              color: Colors.pink,
+            ),
+            Container(
+              margin: EdgeInsets.all(15),
+              width: MediaQuery.of(context).size.width,
+              height: 300,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
