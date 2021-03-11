e2ead941be7a6931daea55ec8fe86d9d026e0f5d
Jacob An <lightyear.thespaceranger@gmail.com>
2021-03-11 22:11:20 +0900

    Update pages

--- artpia/lib/pages/interface.dart
+++ artpia/lib/pages/interface.dart
@@ -12,53 +12,68 @@ class InterfacePage extends StatefulWidget {
   _InterfacePageState createState() => _InterfacePageState();
 }
 
-class _InterfacePageState extends State<InterfacePage> {
+class _InterfacePageState extends State<InterfacePage>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     double _height = MediaQuery.of(context).size.height;
 
     return DefaultTabController(
-        length: 4,
-        initialIndex: 0,
-        child: Scaffold(
-          // floatingActionButton: addProductFAT(context),
-          backgroundColor: Colors.white,
-          body: TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              HomePage(),
-              BrowsePage(),
-              MessagePage(),
-              ProfilePage(),
+      length: 4,
+      initialIndex: 0,
+      child: Scaffold(
+        // floatingActionButton: addProductFAT(context),
+        backgroundColor: Colors.white,
+        body: TabBarView(
+          controller: _controller,
+          physics: NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            HomePage(),
+            BrowsePage(),
+            MessagePage(),
+            ProfilePage(),
+          ],
+        ),
+        bottomNavigationBar: Container(
+          height: _height * 0.1,
+          child: new TabBar(
+            labelColor: Colors.black,
+            indicatorColor: Colors.transparent,
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(
+                icon: Icon(CupertinoIcons.home),
+                //ImageIcon(AssetImage('tab_bar_home')),
+              ),
+              Tab(
+                icon: Icon(CupertinoIcons.search),
+                //ImageIcon(AssetImage('tab_bar_discover'),
+              ),
+              Tab(
+                icon: Icon(CupertinoIcons.conversation_bubble),
+                //ImageIcon(AssetImage('tab_bar_message'),
+              ),
+              Tab(
+                icon: Icon(CupertinoIcons.person),
+                //ImageIcon(AssetImage('tab_bar_my_page'),
+              ),
             ],
           ),
-          bottomNavigationBar: Container(
-            height: _height * 0.1,
-            child: new TabBar(
-              labelColor: Colors.black,
-              indicatorColor: Colors.transparent,
-              unselectedLabelColor: Colors.grey,
-              tabs: [
-                Tab(
-                  icon: Icon(CupertinoIcons.home),
-                  //ImageIcon(AssetImage('tab_bar_home')),
-                ),
-                Tab(
-                  icon: Icon(CupertinoIcons.search),
-                  //ImageIcon(AssetImage('tab_bar_discover'),
-                ),
-                Tab(
-                  icon: Icon(CupertinoIcons.conversation_bubble),
-                  //ImageIcon(AssetImage('tab_bar_message'),
-                  ),
-                Tab(
-                  icon: Icon(CupertinoIcons.person),
-                  //ImageIcon(AssetImage('tab_bar_my_page'),
-                  ),
-              ],
-            ),
-          ),
         ),
+      ),
     );
   }
 }
