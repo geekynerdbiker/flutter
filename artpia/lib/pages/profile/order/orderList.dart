7d830370cb480222f2bd1c050c555de28df604d0
Jacob An <lightyear.thespaceranger@gmail.com>
2021-03-24 20:10:45 +0900

    Create Pages

---
+++ artpia/lib/pages/profile/order/orderList.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderListPage extends StatefulWidget {
+  @override
+  _OrderListPageState createState() => _OrderListPageState();
+}
+
+class _OrderListPageState extends State<OrderListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
