d25609e5c785855fd0aa660f7598f58cb7ad2bb1
Jacob An <lightyear.thespaceranger@gmail.com>
2021-03-26 18:33:15 +0900

    Update pages

---
+++ artpia/lib/pages/order/afterOrder.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AfterOrderPage extends StatefulWidget {
+  @override
+  _AfterOrderPageState createState() => _AfterOrderPageState();
+}
+
+class _AfterOrderPageState extends State<AfterOrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
