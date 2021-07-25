29f8fe94696b4ace883838d870e71abb1ba43d1e
Jacob An <lightyear.thespaceranger@gmail.com>
2021-07-25 16:06:31 +0900

    Create todo list

---
+++ artpia/lib/todo.dart
@@ -0,0 +1,20 @@
+// 25 July (SUN), 2021 Updated
+
+/*
+Front-End
+
+Logo design
+Naming, Branding
+Renewal all of screen design
+*/
+
+/*
+Back-End
+
+Login
+Add artwork
+Order artwork
+Search
+Notification
+Setting
+*/
