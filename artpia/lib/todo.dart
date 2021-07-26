057528158eebf150e6bd81931a49291e3d1ab900
Jacob An <lightyear.thespaceranger@gmail.com>
2021-07-26 17:10:59 +0900

    Update todo

--- artpia/lib/todo.dart
+++ artpia/lib/todo.dart
@@ -1,20 +1,63 @@
 // 25 July (SUN), 2021 Updated
 
 /*
+
 Front-End
 
+App Title
 Logo design
-Naming, Branding
+Branding
 Renewal all of screen design
+
+[ Hidden ]
+- Admin page
+
+[ Level 0 ]
+- Splash
+- Sign in
+
+[ Level 1 ]
+- Home
+- Search
+- Notification ?
+- Profile
+
+[ Level 2 ]
+- Setting
+- Artwork detail
+- Order detail
+
+[ Level 3 ]
+- General Setting
+_ Profile Setting
+- Order result
+
 */
 
 /*
+
 Back-End
 
-Login
+Sign in & up
+- Social Login with Google, Apple, Facebook, Kakao?
+
 Add artwork
+- Upload images to Firestore
+- Upload details to Firebase
+
 Order artwork
+- Upload order detail to Firebase
+- Notify order info to Administrator
+
 Search
+- Recent search keyword
+- Autofill recommend
+
 Notification
+- Notify for order state from admin page
+
 Setting
+- General setting
+- Profile setting
+
 */
