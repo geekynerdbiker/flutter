f7ae6dc57a2f10c2353e9bb476a4db3349d2555b
Jacob An <lightyear.thespaceranger@gmail.com>
2021-07-27 16:54:13 +0900

    Update todo.dart

--- artpia/lib/todo.dart
+++ artpia/lib/todo.dart
@@ -23,14 +23,19 @@ Renewal all of screen design
 - Profile
 
 [ Level 2 ]
+- Artist profile ( Level 1 Profile )
+- Search result
 - Setting
+- Add artwork
 - Artwork detail
-- Order detail
+- Order list
 
 [ Level 3 ]
+- Order artwork
+- Order detail
+- Order result
 - General Setting
 _ Profile Setting
-- Order result
 
 */
 
@@ -38,16 +43,20 @@ _ Profile Setting
 
 Back-End
 
+[ Hidden Level ]
+Admin
+- Send notification of order state
+
+[ Level 0 ]
+Splash
+- Animation logo or image logo
+
 Sign in & up
 - Social Login with Google, Apple, Facebook, Kakao?
 
-Add artwork
-- Upload images to Firestore
-- Upload details to Firebase
-
-Order artwork
-- Upload order detail to Firebase
-- Notify order info to Administrator
+[ Level 1 ]
+Home
+- Exposure algorithm
 
 Search
 - Recent search keyword
@@ -56,8 +65,45 @@ Search
 Notification
 - Notify for order state from admin page
 
+Profile
+-
+
+[ Level 2 ]
+Artist profile
+-
+
+Search result
+-
+
 Setting
 - General setting
 - Profile setting
+
+Add artwork
+- Upload images to Firestore
+- Upload details to Firebase
+
+Artwork detail
+-
+
+Order list
+-
+
+[ Level 3 ]
+Order artwork
+- Upload order detail to Firebase
+- Notify order info to Administrator
+
+Order detail
+-
+
+Order result
+-
+
+General Setting
+-
+
+Profile Setting
+-
 
 */
