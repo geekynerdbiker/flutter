a3c8ddf9318b7b83b4af66d375c26d0fa2ed9220
Jacob An <lightyear.thespaceranger@gmail.com>
2022-03-06 16:50:41 +0900

    update

--- shallcook/lib/page/common/main.dart
+++ shallcook/lib/page/common/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shallcook/page/common/splash.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:shallcook/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:shallcook/page/common/splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
